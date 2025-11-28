0c0ba4b5dcdf6eaab2423a6f88942fb580b7ebec
Jonas Pinheiro <jonas123.projetos@gmail.com>
2025-11-28 18:30:59 -0300

    feat: Implementação dos endpoints de products

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,11 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import cors from 'cors';
-import { prisma } from './utils/prisma.js';
 import { UserController } from './controller/UserController.js';
+import { ProductController } from './controller/ProductController.js';
 
 const userController = new UserController();
-
+const productController = new ProductController();
 const app = express();
 const PORT = 3000;
 app.use(express.json());
@@ -20,5 +20,11 @@ app.get("/branded/users/:id", userController.findById.bind(userController));
 app.post("/branded/users", userController.createUser.bind(userController));
 app.put("/branded/users/:id", userController.updateUser.bind(userController));
 app.delete("/branded/users/:id", userController.deleteById.bind(userController));
+
+app.get("/branded/products", productController.findAll.bind(productController));
+app.get("/branded/products/:id", productController.findById.bind(productController));
+app.post("/branded/products", productController.createProduct.bind(productController));
+app.put("/branded/products/:id", productController.updateProduct.bind(productController));
+app.delete("/branded/products/:id", productController.deleteById.bind(productController));
 
 app.listen(PORT, () => { console.log(`Servidor rodando em http://localhost:${PORT}`)});
